e1379d882238ed79f42bc1973f2736ba7abc24c8
Brian Blosser <brian@slamagency.com>
2016-12-03 16:38:02 -0600

    Made the cat foreground image overlay the background image, restructured the DOM and classnames a bit.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
       event.preventDefault();
       var $headerNavigation = $('.header__navigation');
       var $offCanvasContainer = $('.off-canvas-container');
-      $offCanvasContainer.toggleClass('off-canvas-container--slide-left');
+      // $offCanvasContainer.toggleClass('off-canvas-container--slide-left');
+      var leftValue = $offCanvasContainer.css("left");
+      if ( leftValue == "0px" ) {
+        $offCanvasContainer.css("left", "-50%");
+      } else {
+        $offCanvasContainer.css("left", "0px")
+      }
   });
 });
